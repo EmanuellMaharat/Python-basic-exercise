4502f7c32448a8da9590451cb5d92662db0f31d1
mahar <maharatemanuel@gmail.com>
2022-03-09 16:03:27 +0200

    Add files via upload

--- exercise.py
+++ exercise.py
@@ -1,3 +1,4 @@
+#1 uneven numbers from 1-20 while loop
 maximum = int(input(" Please Enter the Maximum Value : "))
 
 number = 1
@@ -6,3 +7,10 @@ while number <= maximum:
     if(number % 2 != 0):
         print("{0}".format(number))
     number = number + 1
+
+#2 uneven numbers from 1-20 for loop
+maximum = int(input(" Please Enter any Maximum Value : "))
+
+for number in range(1, maximum + 1):
+    if(number % 2 != 0):
+        print("{0}".format(number))
