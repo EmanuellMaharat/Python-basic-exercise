44f7b124606613beb0ffda54a8c7c34063c17ffc
mahar <maharatemanuel@gmail.com>
2022-03-16 12:08:51 +0200

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,41 @@
+#1 I love python 5 times
+for i in range (5):
+    print ("i love python")
+
+#2 creating an empty list
+lst = []
+# number of elements as input
+n = int(input("Enter number of elements : "))
+
+for i in range(0, n):
+    list = int(input())
+
+    lst.append(list)  # adding the element
+
+print(lst)
+
+#3
+for i in range (11)
+    n = int(input("please enter a num"))
+    ls.append(n)
+    for i in LS :
+        x=i/11
+        divided_11.append(x)
+        if i%2==0:
+            even_nums.append(i)
+        if i%3==0 :
+            divided_3+=1
+        if i>10 :
+             bigger_10+=1
+        if i<10:
+             lower_10 +=1
+
+#4 Python code to check if 2 numbers are same
+def areSame(a, b):
+	if (not(a - b)):
+		print ("Same")
+	else:
+		print ("Not Same")
+# Driver code
+areSame(20, 20)
+
