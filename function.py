b997b9a2af849918161a4bdb0edd92cab0e5c8db
mahar <maharatemanuel@gmail.com>
2022-03-17 14:33:47 +0200

    Add files via upload

---
+++ function.py
@@ -0,0 +1,32 @@
+#save names
+def save_user(**user):
+    print (user)
+
+save_user(id=1, name="john", age=22)
+
+#list of numbers
+def list(*numbers):
+    print(numbers)
+
+list(2,3,4,5)
+
+
+#Multiply numbers
+def Multiply(num1, num2):
+    return num1 * num2
+
+print(Multiply(2, 3))
+
+
+#print first and last name
+def greet(first_name , last_name)
+    print (f"Hi {first_name}{last_name}")
+    print ("Welcome")
+
+greet(Emanuel,maharat)
+
+#addition
+def addition(num1, num2):
+    return num1 + num2
+
+print(addition(2, 3))
