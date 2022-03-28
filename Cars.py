65976d4f8ac6ff109ca5dd0ea183a5a3cf4cddde
mahar <maharatemanuel@gmail.com>
2022-03-28 10:38:35 +0300

    Add files via upload

---
+++ Cars.py
@@ -0,0 +1,9 @@
+class Cars:
+    def __init__(self,model,color,num_wheels, multimedia):
+        self.model = model
+        self.color = color
+        self.num_wheels = num_wheels
+        self.multimedia = multimedia
+
+
+
---
+++ Swift.py
@@ -0,0 +1,4 @@
+from Cars import Cars
+
+suzuki_swift = Cars (2014 ,'Black' , 4 , 'android' )
+print(suzuki_swift.color)
