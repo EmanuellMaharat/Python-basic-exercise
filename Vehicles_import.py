2ed143e3fe488d36f0cd332d8f8723da1f688616
mahar <maharatemanuel@gmail.com>
2022-03-28 11:42:14 +0300

    Add files via upload

--- Vehicles_import.py
+++ Vehicles_import.py
@@ -1,3 +1,23 @@
+class Cars:
+    def __init__(self,model,color,num_wheels, multimedia):
+        self.model = model
+        self.color = color
+        self.num_wheels = num_wheels
+        self.multimedia = multimedia
+
+class Bycicle:
+    def __init__(self,weight,color, cost ):
+        self.weight = weight
+        self.color_bycicle = color
+        self.cost = cost
+
+class Airplane:
+    def __init__(self ,wings , model , cost ):
+        self.wings = wings
+        self.model_airplane = model
+        self.cost_airplane = cost
+
+
 from Vehicles import Cars
 
 suzuki_swift = Cars (2014 ,'Black' , 4 , 'android' )
